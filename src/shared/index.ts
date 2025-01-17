d0d3d606c5097eae8499370806d779e337fb8b9b
Shivanshi Tyagi <shivanshityagi@icloud.com>
2025-01-17 17:38:26 +0700

    add basic versions

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,9 +1,15 @@
 import { transferTools } from "./transfer";
 import { ensTools } from "./ens";
 import { dexscreenerTools } from "./dexscreener";
+import { rpcTools } from "./rpc";
 
 const allTools = () => {
-  return [...ensTools(), ...transferTools(), ...dexscreenerTools()];
+  return [
+    ...ensTools(),
+    ...transferTools(),
+    ...dexscreenerTools(),
+    ...rpcTools(),
+  ];
 };
 
 export { allTools };
---
+++ src/shared/rpc/tools.ts
@@ -0,0 +1,141 @@
+import { z } from "zod";
+import { createTool } from "../client";
+import { Address, formatEther, Hex, formatUnits, parseEther } from "viem";
+import { arbitrum, base, mainnet, optimism } from "viem/chains";
+
+const supportedChains = [mainnet, base, arbitrum];
+
+const getBalance = createTool({
+  name: "getBalance",
+  description: "Get the ETH balance for an address",
+  supportedChains,
+  parameters: z.object({
+    address: z.string(),
+    chainId: z.number().optional(),
+    formatEth: z.boolean().optional(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    const balance = await publicClient.getBalance({
+      address: args.address as Address,
+    });
+    return args.formatEth ? formatEther(balance) : balance.toString();
+  },
+});
+
+const getCode = createTool({
+  name: "getCode",
+  description: "Get the bytecode of an address",
+  supportedChains,
+  parameters: z.object({
+    address: z.string(),
+    chainId: z.number().optional(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    return publicClient.getCode({ address: args.address as Address });
+  },
+});
+
+const getTransactionCount = createTool({
+  name: "getTransactionCount",
+  description: "Get the number of transactions sent from an address",
+  supportedChains,
+  parameters: z.object({
+    address: z.string(),
+    chainId: z.number().optional(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    return publicClient.getTransactionCount({
+      address: args.address as Address,
+    });
+  },
+});
+
+// Block Tools
+const getBlock = createTool({
+  name: "getBlock",
+  description: "Get information about a block",
+  supportedChains,
+  parameters: z.object({
+    blockNumber: z.number().optional(),
+    blockHash: z.string().optional(),
+    chainId: z.number(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    if (args.blockHash) {
+      return publicClient.getBlock({ blockHash: args.blockHash as Hex });
+    }
+
+    if (args.blockNumber) {
+      return publicClient.getBlock({
+        blockNumber: BigInt(args.blockNumber),
+      });
+    }
+
+    return publicClient.getBlock();
+  },
+});
+
+const getBlockNumber = createTool({
+  name: "getBlockNumber",
+  description: "Get the current block number",
+  supportedChains,
+  parameters: z.object({
+    chainId: z.number().optional(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    return publicClient.getBlockNumber();
+  },
+});
+
+// Gas Tools
+const getGasPrice = createTool({
+  name: "getGasPrice",
+  description: "Get the current gas price",
+  supportedChains,
+  parameters: z.object({
+    chainId: z.number().optional(),
+    formatGwei: z.boolean().optional(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    const gasPrice = await publicClient.getGasPrice();
+    return args.formatGwei ? formatUnits(gasPrice, 9) : gasPrice.toString();
+  },
+});
+
+const estimateGas = createTool({
+  name: "estimateGas",
+  description: "Estimate gas for a transaction",
+  supportedChains,
+  parameters: z.object({
+    to: z.string(),
+    value: z.string().optional(),
+    data: z.string().optional(),
+    chainId: z.number().optional(),
+  }),
+  execute: async (client, args) => {
+    const publicClient = client.getPublicClient(args.chainId);
+    const from = await client.getAddress();
+    return publicClient.estimateGas({
+      account: from,
+      to: args.to,
+      value: args.value ? parseEther(args.value) : undefined,
+      data: args.data as `0x${string}` | undefined,
+    });
+  },
+});
+
+export {
+  getBalance,
+  getCode,
+  getTransactionCount,
+  getBlock,
+  getBlockNumber,
+  getGasPrice,
+  estimateGas,
+};
